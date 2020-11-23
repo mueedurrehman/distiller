02b4a8a563f010309423e73aa629d32fca0f3ec7
Mueed Ur Rehman <mueedurrehman@users.noreply.github.com>
2020-11-22 23:05:04 -0500

    Changed normalization for CIFAR dataloaders

--- distiller/apputils/data_loaders.py
+++ distiller/apputils/data_loaders.py
@@ -148,7 +148,7 @@ def cifar10_get_datasets(data_dir, load_train=True, load_test=True):
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         ])
 
         train_dataset = datasets.CIFAR10(root=data_dir, train=True,
@@ -156,10 +156,7 @@ def cifar10_get_datasets(data_dir, load_train=True, load_test=True):
 
     test_dataset = None
     if load_test:
-        test_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+        test_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
         test_dataset = datasets.CIFAR10(root=data_dir, train=False,
                                         download=True, transform=test_transform)
